fe9d4bf45eb0a74d8d1af107cf5eb6bab4b67ff8
tomasoignons <tomasoignons@gmail.com>
2024-02-20 11:15:27 +0100

    toute la class Point.java

--- src/ch/epfl/chacun/Points.java
+++ src/ch/epfl/chacun/Points.java
@@ -4,29 +4,42 @@ public final class Points {
     private Points() {}
 
     public int forClosedForest (int tileCount, int mushroomGroupCount) {
-        if(tileCount < 1 || mushroomGroupCount < 0)
-            throw new IllegalArgumentException();
+        checkArgument(1, tileCount, mushroomGroupCount);
+        return (2*tileCount + 3*mushroomGroupCount);
     };
 
     public int forClosedRiver (int tileCount, int fishCount) {
-        if(tileCount < 1 || fishCount < 0)
-            throw new IllegalArgumentException();
+        checkArgument(1, tileCount, fishCount);
+        return (tileCount + fishCount);
     };
 
     public int forMeadow (int mammothCount, int aurochsCount, int deerCount) {
-        if(mammothCount < 0 || aurochsCount < 0 || deerCount < 0)
-            throw new IllegalArgumentException();
+        checkArgument(0, mammothCount, aurochsCount, deerCount);
+        return (3*mammothCount + 2*aurochsCount + deerCount);
     };
     public int forRiverSystem (int fishCount) {
-        if(fishCount < 0)
-            throw new IllegalArgumentException();
+        checkArgument(0, fishCount);
+        return (fishCount);
     };
     public int forLogboat (int lakeCount) {
-        if(lakeCount < 0)
-            throw new IllegalArgumentException();
+        checkArgument(0, lakeCount);
+        return (2*lakeCount);
     };
     public int forRaft (int lakeCount) {
-        if(lakeCount < 0)
-            throw new IllegalArgumentException();
+        checkArgument(0, lakeCount);
+        return (lakeCount);
     };
+    /**
+     * This is a method to check if the arguments of the Point class are valid
+     * @param value the value to check for the first argument
+     * @param arguments the list of argument, the first argument is going to be checked against the value, and the other must be positive (or null)
+     */
+    private void checkArgument (int value, int... arguments) {
+        if(arguments.length < 1 || arguments[0] < value)
+            throw new IllegalArgumentException();
+        for(int i = 1; i < arguments.length; i++) {
+            if(arguments[i] < 0)
+                throw new IllegalArgumentException();
+        }
+    }
 }
