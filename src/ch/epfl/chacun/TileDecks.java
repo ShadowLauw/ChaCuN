329cd2eb274c18526988556bf74d61d6313b5198
Laura Paraboschi <laura.paraboschi@epfl.ch>
2024-02-22 17:31:16 +0000

    Ajout des @param + modif constructeur + modif des méthodes (switch simplifié sans le throw) + supp removeFirst (appel direct) + modif TwithTopTIleDrawnUntil

--- src/ch/epfl/chacun/TileDecks.java
+++ src/ch/epfl/chacun/TileDecks.java
@@ -7,7 +7,11 @@ import java.util.function.Predicate;
  * Represents the different decks of tiles
  *
  * @author Laura Paraboschi (364161)
- * @Author Emmanuel Omont (372632)
+ * @author Emmanuel Omont (372632)
+ *
+ * @param startTiles (List<Tile>) the start tiles deck (composed of only the start tile)
+ * @param normalTiles (List<Tile>) the normal tiles deck
+ * @param menhirTiles (List<Tile>) the menhir tiles deck
  */
 public record TileDecks(List<Tile> startTiles, List<Tile> normalTiles, List<Tile> menhirTiles) {
     /**
@@ -16,8 +20,7 @@ public record TileDecks(List<Tile> startTiles, List<Tile> normalTiles, List<Tile
      * @param normalTiles (List<Tile>) the normal tiles deck
      * @param menhirTiles (List<Tile>) the menhir tiles deck
      */
-    //je crois que je comprend pas comment implémenter un constructeur "par défaut"
-    public TileDecks (List<Tile> startTiles, List<Tile> normalTiles, List<Tile> menhirTiles) {
+    public TileDecks {
         this.startTiles = List.copyOf(startTiles);
         this.normalTiles = List.copyOf(normalTiles);
         this.menhirTiles = List.copyOf(menhirTiles);
@@ -28,74 +31,39 @@ public record TileDecks(List<Tile> startTiles, List<Tile> normalTiles, List<Tile
      * @return (int) the size of the deck of the given kind
      */
     public int deckSize(Tile.Kind kind) {
-        switch (kind) {
-            case START -> {
-                return startTiles.size();
-            }
-            case NORMAL -> {
-                return normalTiles.size();
-            }
-            case MENHIR -> {
-                return menhirTiles.size();
-            }
-        }
-        throw new IllegalArgumentException();
+        return switch(kind) {
+            case START -> startTiles.size();
+            case NORMAL -> normalTiles.size();
+            case MENHIR -> menhirTiles.size();
+        };
     }
     /**
      * Returns the top tile of the deck of the given kind
      * @param kind (Tile.Kind) the kind of deck
      * @return (Tile) the top tile of the deck of the given kind
      */
-    //attention, potentiellement il veut qu'on l'implémente en inversé, donc faire gaffe au changement d'énnoncé jusqu'à la dernière minute
     public Tile topTile (Tile.Kind kind) {
-        switch (kind) {
-            case START -> {
-                return startTiles.get(0);
-            }
-            case NORMAL -> {
-                return normalTiles.get(0);
-            }
-            case MENHIR -> {
-                return menhirTiles.get(0);
-            }
-        }
-        throw new IllegalArgumentException();
+        return switch(kind) {
+            case START -> startTiles.isEmpty() ? startTiles.getFirst() : null;
+            case NORMAL -> normalTiles.isEmpty() ? startTiles.getFirst() : null;
+            case MENHIR -> menhirTiles.isEmpty() ? startTiles.getFirst() : null;
+        };
     }
     /**
      * Returns a new TileDecks with the top tile of the given kind removed
      * @param kind (Tile.Kind) the kind of deck
      * @return (TileDecks) a new TileDecks with the top tile of the given kind removed
+     * @throws IllegalArgumentException if the deck of the given kind is empty
      */
-    //ici c'est bancal vu que j'utilise la méthode remove des arrayList je crois, et pas des lists
     public TileDecks withTopTileDrawn(Tile.Kind kind) {
         Preconditions.checkArgument(deckSize(kind) > 0);
-        switch (kind) {
-            case START -> {
-                return new TileDecks(removeFirstElementDeck(this.startTiles), this.normalTiles, this.menhirTiles);
-            }
-            case NORMAL -> {
-                return new TileDecks(this.startTiles, removeFirstElementDeck(this.normalTiles), this.menhirTiles);
-            }
-            case MENHIR -> {
-                return new TileDecks(this.startTiles, this.normalTiles, removeFirstElementDeck(this.menhirTiles));
-            }
-            default -> {
-                throw new IllegalArgumentException();
-            }
-        }
+        return switch (kind) {
+            case START -> new TileDecks(this.startTiles.remove(), this.normalTiles, this.menhirTiles);
+            case NORMAL -> new TileDecks(this.startTiles, this.normalTiles.remove() this.menhirTiles);
+            case MENHIR -> new TileDecks(this.startTiles, this.normalTiles, this.menhirTiles.remove());
+        };
     }
 
-    /**
-     * Returns a List of Tile with the first element removed
-     * @param initialDeck (List<Tile>) the initial deck
-     * @return (List<Tile>) a List of Tile with the first element removed
-     */
-    private List<Tile> removeFirstElementDeck(List<Tile> initialDeck) {
-        Preconditions.checkArgument(!initialDeck.isEmpty());
-        List<Tile> newDeck = List.copyOf(initialDeck);
-        newDeck.remove(0);
-        return newDeck;
-    }
     /**
      * Returns a new TileDecks with the top tile of the given kind drawn until the given predicate is satisfied
      * @param kind (Tile.Kind) the kind of deck
@@ -104,8 +72,10 @@ public record TileDecks(List<Tile> startTiles, List<Tile> normalTiles, List<Tile
      */
     public TileDecks withTopTileDrawnUntil(Tile.Kind kind, Predicate<Tile> predicate) {
         TileDecks newTileDeck = new TileDecks(this.startTiles, this.normalTiles, this.menhirTiles);
-        while (!predicate.test(newTileDeck.topTile(kind))){
+        Tile topTile = newTileDeck.topTile(kind);
+        while (topTile != null && !predicate.test(topTile)){
             newTileDeck = withTopTileDrawn(kind);
+            topTile = newTileDeck.topTile(kind);
         }
         return newTileDeck;
     }
