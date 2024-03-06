cb2f1ebe6c1a86598376d979a04f1789bfd26129
tomasoignons <tomasoignons@gmail.com>
2024-03-06 17:39:50 +0100

    tests de Emmanuel

---
+++ src/ch/epfl/chacun/ZonePartitions.java
@@ -0,0 +1,47 @@
+package ch.epfl.chacun;
+
+import java.util.Set;
+
+public record ZonePartitions (ZonePartition<Zone.Forest> forests, ZonePartition<Zone.Meadow> meadow, ZonePartition<Zone.River> rivers, ZonePartition<Zone.Water> riverSystems){
+
+    public final static ZonePartitions EMPTY = new ZonePartitions(new ZonePartition<>(Set.of()), new ZonePartition<>(Set.of()), new ZonePartition<>(Set.of()), new ZonePartition<>(Set.of()));
+
+    public final class Builder {
+        private ZonePartition.Builder<Zone.Forest> forests;
+        private ZonePartition.Builder<Zone.Meadow> meadow;
+        private ZonePartition.Builder<Zone.River> rivers;
+        private ZonePartition.Builder<Zone.Water> riverSystems;
+
+        Builder (ZonePartitions initial) {
+
+        }
+
+
+        public void addTile (Tile tile){
+
+        }
+        public void connectSide (TileSide s1, TileSide s2){
+
+        }
+
+        public void addInitialOccupant (PlayerColor player, Occupant.Kind occupantKind, Zone occupiedZone){
+
+        }
+
+        public void removePawn(PlayerColor player, Zone occupiedZone) {
+
+        }
+
+        public void clearGatherers(Area<Zone.Forest> forest) {
+
+        }
+
+        public void clearFishers(Area<Zone.River> river) {
+
+        }
+
+        public ZonePartitions build() {
+            return null;
+        }
+    }
+}
