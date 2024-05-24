bc3ddf7b09fcf5ffc2d0e6e15fe7204523b06d8d
Lauw <Laura.paraboschi@hotmail.com>
2024-05-24 11:52:17 +0200

    Fix

--- src/ch/epfl/chacun/Tiles.java
+++ src/ch/epfl/chacun/Tiles.java
@@ -6,8 +6,8 @@ import java.util.List;
 
 public final class Tiles {
     private Tiles() {}
-
-    public static final List<Tile> TILES = createTiles();
+    static List<Integer> idToPlay = List.of(1,2,3,4,56, 92);
+    public static final List<Tile> TILES = new ArrayList<>(createTiles().stream().filter(t -> idToPlay.contains(t.id())).toList());
 
     private static List<Tile> createTiles() {
         ArrayList<Tile> tiles = new ArrayList<>();
